bd7801011361b7290bdecc062232a169230af824
leiliu <llei1025@gmail.com>
2019-03-26 02:56:11 -0400

    tag element improvement

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,113 @@
 // dl-menu options
-$(function () {
-    $('#dl-menu').dlmenu({
+$(function() {
+    $("#dl-menu").dlmenu({
         animationClasses: {
-            classin: 'dl-animate-in',
-            classout: 'dl-animate-out'
+            classin: "dl-animate-in",
+            classout: "dl-animate-out"
         }
     });
 });
 // Need this to show animation when go back in browser
-window.onunload = function () {};
+window.onunload = function() {};
 
 // Add lightbox class to all image links
-$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+$(
+    "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
+).addClass("image-popup");
 
 // FitVids options
-$(function () {
+$(function() {
     $(".content").fitVids();
 });
 
-// All others
-$(document).ready(function () {
-    //preloader nanimation
-    preloader();
+/*
+$(function(){
+    const content = document.querySelector('.post__article');
 
+    scrollnav.init(content, { 
+      debug: false
+    });
+});
+*/
+
+// All others
+$(document).ready(function() {
+    //preloader animation
+    preloader();
     //tags page
+    tagsPage();
+    //do not show javascript tag if js exists due to limitaion of space
+    hideJavascriptTag();
+    // go up button
+    goUpBtn();
+    //image pop up
+    imagePopup();
+    //material button
+    $(".ripple-btn").rkmd_rippleEffect();
+});
+
+//loader
+function preloader() {
+    $("#preloader").hide();
+    $(".loading").css("visibility", "visible");
+
+    //all pages tile fade in
+    $(".post-title").addClass("animated fadeIn");
+    $(".post-content").addClass("animated bounceInUp");
+}
+
+function hideJavascriptTag() {
+    var links = $(".post-list-tags a");
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        var isSpanHasJs = element.firstElementChild.textContent
+            .toLowerCase()
+            .includes("javascript");
+        if (isSpanHasJs) {
+            $(element).hide();
+        }
+    }
+}
+
+//material button function
+(function($) {
+    $.fn.rkmd_rippleEffect = function() {
+        var btn, self, ripple, size, rippleX, rippleY, eWidth, eHeight;
+        btn = $(this).not("[disabled], .disabled");
+        btn.on("mousedown", function(e) {
+            self = $(this);
+            // Disable right click
+            if (e.button === 2) {
+                return false;
+            }
+            if (self.find(".ripple").length === 0) {
+                self.prepend('<span class="ripple"></span>');
+            }
+            ripple = self.find(".ripple");
+            ripple.removeClass("animated");
+            eWidth = self.outerWidth();
+            eHeight = self.outerHeight();
+            size = Math.max(eWidth, eHeight);
+            ripple.css({
+                width: size,
+                height: size
+            });
+            rippleX = parseInt(e.pageX - self.offset().left) - size / 2;
+            rippleY = parseInt(e.pageY - self.offset().top) - size / 2;
+            ripple
+                .css({
+                    top: rippleY + "px",
+                    left: rippleX + "px"
+                })
+                .addClass("animated");
+            setTimeout(function() {
+                ripple.remove();
+            }, 800);
+        });
+    };
+})(jQuery);
+
+function tagsPage() {
     if (window.location.pathname.includes('tags')) {
         var tagName = window.location.hash.substr(1);
         $("article[id=\'" + tagName + "\']").show().addClass('animated fadeIn');
@@ -36,94 +120,35 @@ $(document).ready(function () {
         console.log(tagName);
         $("article[id=\'" + tagName + "\']").show().addClass('animated fadeIn').siblings('article').hide();
     });
+}
 
-    //do not show javascript tag if js exists due to limitaion of space
-    hideJavascriptTag();
-
-    // go up button
-    $.goup({
-        trigger: 500,
-        bottomOffset: 10,
-        locationOffset: 20,
-        containerRadius: 0,
-        containerColor: '#fff',
-        arrowColor: '#000',
-        goupSpeed: 'normal'
-    });
-    $('.image-popup').magnificPopup({
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
+function imagePopup() {
+    $(".image-popup").magnificPopup({
+        type: "image",
+        tLoading: "Loading image #%curr%...",
         gallery: {
             enabled: true,
             navigateByImgClick: true,
             preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+            tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
         },
         removalDelay: 300, // Delay in milliseconds before popup is removed
-        // Class that is added to body when popup is open. 
+        // Class that is added to body when popup is open.
         // make it unique to apply your CSS animations just to this exact popup
-        mainClass: 'mfp-fade'
+        mainClass: "mfp-fade"
     });
-
-    //material button 
-    $('.ripple-btn').rkmd_rippleEffect();
-});
-
-//loader
-function preloader() {
-    $('#preloader').hide();
-    $('.loading').css('visibility', 'visible');
-
-    //all pages tile fade in
-    $('.post-title').addClass('animated fadeIn');
-    $('.post-content').addClass('animated bounceInUp');
-};
-
-function hideJavascriptTag() {
-    var links = $('.post-list-tags a');
-    for (let i = 0; i < links.length; i++) {
-        const element = links[i];
-        var isSpanHasJs = element.firstElementChild.textContent.toLowerCase().includes('javascript')
-        if (isSpanHasJs) {
-            $(element).hide();
-        }
-    }
 }
 
-//material button function
-(function ($) {
-    $.fn.rkmd_rippleEffect = function () {
-        var btn, self, ripple, size, rippleX, rippleY, eWidth, eHeight;
-        btn = $(this).not('[disabled], .disabled');
-        btn.on('mousedown', function (e) {
-            self = $(this);
-            // Disable right click
-            if (e.button === 2) {
-                return false;
-            }
-            if (self.find('.ripple').length === 0) {
-                self.prepend('<span class="ripple"></span>');
-            }
-            ripple = self.find('.ripple');
-            ripple.removeClass('animated');
-            eWidth = self.outerWidth();
-            eHeight = self.outerHeight();
-            size = Math.max(eWidth, eHeight);
-            ripple.css({
-                'width': size,
-                'height': size
-            });
-            rippleX = parseInt(e.pageX - self.offset().left) - (size / 2);
-            rippleY = parseInt(e.pageY - self.offset().top) - (size / 2);
-            ripple.css({
-                'top': rippleY + 'px',
-                'left': rippleX + 'px'
-            }).addClass('animated');
-            setTimeout(function () {
-                ripple.remove();
-            }, 800);
-        });
-    };
-}(jQuery));
+function goUpBtn() {
+    $.goup({
+        trigger: 500,
+        bottomOffset: 10,
+        locationOffset: 20,
+        containerRadius: 0,
+        containerColor: "#fff",
+        arrowColor: "#000",
+        goupSpeed: "normal"
+    });
+}
